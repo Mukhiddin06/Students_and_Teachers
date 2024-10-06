56bd8ba44c6b8dbd09d9c451559b24fff2814024
Mukhiddin06 <muhiddinbegiboyev@gmail.com>
2024-10-06 10:36:21 +0500

    Change Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,56 @@
 import { Button, Input, Select } from 'antd'
 import './App.css'
 import Item from './components/Item'
-import useFetch from './hook/useFetch'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const students = useFetch("/students")
-  const teachers = useFetch("/teachers")
+  const [students, setStudents] = useState([])
+  const [teachers, setTeachers] = useState([])
   const [statusUser, setStatusUser] = useState("1")
+  const [refreshStudent, setRefreshStudent] = useState(false)
+  const [refreshTeacher, setRefreshTeacher] = useState(false)
+  function handleSubmit(e){
+    e.preventDefault()
+    const data ={
+      name:e.target.name.value,
+      surname:e.target.surname.value,
+    }
+    if(statusUser == "1"){
+      data.study = e.target.jobOrStudy.value
+      fetch("http://localhost:3000/students", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+      }).then(res => {
+        setRefreshStudent(!refreshStudent)
+      })
+    }
+    else{
+      data.job = e.target.jobOrStudy.value
+      fetch("http://localhost:3000/teachers", {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+      }).then(res => {
+        setRefreshTeacher(!refreshTeacher)
+      })
+    }
+  }
 
+  useEffect(() => {
+    fetch("http://localhost:3000/students").then(res => res.json())
+    .then(data => setStudents(data))
+  }, [refreshStudent])
+
+
+  useEffect(() => {
+    fetch("http://localhost:3000/teachers").then(res => res.json())
+    .then(data => setTeachers(data))
+  }, [refreshTeacher])
 
   return (
     <>
-      <form className='w-[600px] p-5 rounded-md bg-slate-300 mx-auto mb-10 mt-5'>
+      <form onSubmit={handleSubmit} autoComplete='off' className='w-[600px] p-5 rounded-md bg-slate-300 mx-auto mb-10 mt-5'>
         <div className='flex items-center justify-between space-x-5'>
           <div className='flex flex-col w-[50%] space-y-5'>
             <Select
@@ -41,14 +79,14 @@ function App() {
             <Input name='surname' className='w-full' size='large' allowClear type='text' placeholder='Enter surname' />
           </div>
         </div>
-        <Button className='w-full mt-5' size='large' type='primary'>Add {statusUser == "1" ? "Student" : "Teacher"}</Button> 
+        <Button htmlType='submit' className='w-full mt-5' size='large' type='primary'>Add {statusUser == "1" ? "Student" : "Teacher"}</Button> 
       </form>
       <div className='flex justify-center gap-20 p-10'>
         <ul className='bg-slate-300 p-5 rounded-lg space-y-5'>
-          {students.map(item => <Item key={item.id} item={item} />)}
+          {students.map(item => <Item refreshStudent={refreshStudent} setRefreshStudent={setRefreshStudent} refreshTeacher={refreshTeacher} setRefreshTeacher={setRefreshTeacher} key={item.id} item={item} />)}
         </ul>
         <ul className='bg-slate-300 p-5 rounded-lg space-y-5'>
-          {teachers.map(item => <Item key={item.id} item={item} />)}
+          {teachers.map(item => <Item refreshStudent={refreshStudent} setRefreshStudent={setRefreshStudent} refreshTeacher={refreshTeacher} setRefreshTeacher={setRefreshTeacher} key={item.id} item={item} />)}
         </ul>
       </div>
     </>
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,22 +2,41 @@ import React from 'react';
 import { DeleteOutlined, EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import { Avatar, Card } from 'antd';
 const { Meta } = Card;
-const Item = ({item}) => (
-  <Card
-    style={{
-      width: 300,
-    }}
-    actions={[
-      <DeleteOutlined key="delete" />,
-      <EditOutlined key="edit" />,
-      <EllipsisOutlined key="ellipsis" />,
-    ]}
-  >
-    <Meta
-      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
-      title={`${item.name} - ${item.surname}`}
-      description={item.study ? item.study : item.job}
-    />
-  </Card>
-);
+const Item = ({ item, refreshStudent, refreshTeacher, setRefreshStudent, setRefreshTeacher }) => {
+    function handleDeleteUser(user) {
+        if(user.job){
+            fetch(`http://localhost:3000/teachers/${user.id}`, { method:"DELETE"})
+            .then(res => {
+                setRefreshTeacher(!refreshTeacher)
+            })
+        }
+        else{
+            fetch(`http://localhost:3000/students/${user.id}`, { method:"DELETE"})
+            .then(res => {
+                setRefreshStudent(!refreshStudent)
+            })
+        }
+    }
+
+    return (
+        <>
+            <Card
+                style={{
+                    width: 300,
+                }}
+                actions={[
+                    <DeleteOutlined onClick={() => handleDeleteUser(item)} className='hover:!text-red-500 scale-125' key="delete" />,
+                    <EditOutlined className='scale-125' key="edit" />,
+                    <EllipsisOutlined className='scale-125' key="ellipsis" />,
+                ]}
+            >
+                <Meta
+                    avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+                    title={`${item.name} - ${item.surname}`}
+                    description={item.study ? item.study : item.job}
+                />
+            </Card>
+        </>
+    )
+};
 export default Item;
